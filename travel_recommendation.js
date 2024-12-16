6fa6de0b62ec38353f371b41346c111d4bbc5d61
Alisha Lamichhane <alisha.coder@gmail.com>
2024-12-16 19:53:56 +0545

    show more than 1 results

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,35 +5,40 @@ $('#btnSearch').on('click', function() {
         type: 'GET',
         dataType: 'json',
         success: function(data) {
-            let results = [];
-
+            let results = {
+                beaches: [],
+                temples: [],
+                countries: []
+            };
+        
+            data.beaches.forEach(beach => {
+                if (beach.name.toLowerCase().includes(searchQuery) && results.beaches.length < 2) {
+                    results.beaches.push(beach);
+                }
+            });
+    
+            data.temples.forEach(temple => {
+                if (temple.name.toLowerCase().includes(searchQuery) && results.temples.length < 2) {
+                    results.temples.push(temple);
+                }
+            });
+        
             data.countries.forEach(country => {
                 country.cities.forEach(city => {
-                    if (city.name.toLowerCase().includes(searchQuery)) {
-                        results.push(city);
+                    if (city.name.toLowerCase().includes(searchQuery) && results.countries.length < 2) {
+                        results.countries.push(city);
                     }
                 });
             });
-
-            data.temples.forEach(temple => {
-                if (temple.name.toLowerCase().includes(searchQuery)) {
-                    results.push(temple);
-                }
-            });
-
-            data.beaches.forEach(beach => {
-                if (beach.name.toLowerCase().includes(searchQuery)) {
-                    results.push(beach);
-                }
-            });
-
         
-            if (results.length > 0) {
-                displayResults(results);
+            const allResults = [...results.beaches, ...results.temples, ...results.countries];
+            if (allResults.length > 0) {
+                displayResults(allResults);
             } else {
                 alert('No results found!');
             }
-        },
+        }
+        ,
         error: function() {
             alert('Error fetching data');
         }
